136e1955853705c993e56798b46594506916e7ea
“madhi” <madhiseelan@gmail.com>
2025-12-06 08:24:27 -0500

    relational operator

---
+++ lesson4.js
@@ -0,0 +1,29 @@
+//Relational or Comparison operators
+
+// > - more than 
+// < - less than
+// >= - m,ore than or equal to
+// <= - less than or equal to
+
+// It returns the boolean value - true or false
+
+var result = 10 > 5;
+console.log(result)
+
+var result1 = 4 < 5;
+console.log(result1)
+
+var result3 = 5 < 5;
+console.log(result3);
+
+var result4 = 5 <= 5;
+console.log(result4);
+
+var result5= 5!= 5;
+console.log(result5);
+
+//Equality operators
+var x = 1
+console.log(x == '1'); //loose equality operator. this compares a direct value, not the data type
+console.log(x === '1'); //strict equality operator. this compares both value and data type. here 1 is a string as it is in single quotes
+
